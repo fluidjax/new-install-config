4520a28bafed57dfbb785f4e3f485b3cad85c690
Christopher Morris <chris@morris.net>
2019-01-19 09:30:37 +0000

    Tweak

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@ sudo apt-get update
 sudo apt-get -y install zsh tmux net-tools
 
 #oh-my-zsh
+rm -rf ~/.oh-my-zsh
+rm ~/.tmux.conf
+rm ~/.zshrc
+rm ~/aliases
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9k
 
