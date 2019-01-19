348621eafbd177e4ab70d88d64d41e7bbbba1bff
Christopher Morris <chris@morris.net>
2019-01-19 09:28:56 +0000

    Add aliases

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@ sudo apt-get -y install zsh tmux net-tools
 
 #oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9K
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9k
 
 cp ~/new-install-config/.tmux.conf ~
 cp ~/new-install-config/.zshrc ~
 cp ~/new-install-config/aliases ~
 cp ~/new-install-config/oh-my-zsh.sh ~/.oh-my-zsh/
 
+echo 'alias t="tmux new -A -s chris"' >> ~/.bash_aliases
+source ~/.bash_aliases
