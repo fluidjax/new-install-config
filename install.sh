9ac5d7c11d7e88e295b076a6a780c93b7296d65a
Christopher Morris <chris@morris.net>
2019-01-19 09:21:18 +0000

    Fix Install

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
+sudo apt-get update
+sudo apt-get -y install zsh tmux net-tools
 
+#oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9K
 
 cp ~/new-install-config/.tmux.conf ~
 cp ~/new-install-config/.zshrc ~
 cp ~/new-install-config/aliases ~
-cp ~/new-install-config/oh-my-zsh.sh ~
+cp ~/new-install-config/oh-my-zsh.sh ~/.oh-my-zsh/
 
